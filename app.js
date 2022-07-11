e6db2d5b3a5d05bd719395880b41f15e8e0948f5
Erian <erian.arroyo.herrera@gmail.com>
2022-07-11 18:20:27 -0400

    rebrand

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-/*still learning javascript lol*/
+const fades = document.querySelectorAll(".fade-in");
+const appearOptions = {
+    threshold: 1,
+};
+
+const appearOnScroll = new IntersectionObserver(function( entries, appearOnScroll) {
+entries.forEach(entry =>{
+    if (!entry.isIntersecting){
+        return;
+    } else{
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+    }
+})
+}, appearOptions);
+
+fades.forEach(fader =>{
+    appearOnScroll.observe(fader);
+})
